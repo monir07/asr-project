12cf51c6a15b15b684e109e6c34cb6fed16f7624
monir07 <monirulslm7@gmail.com>
2024-03-05 22:51:31 +0600

    xhtml2pdf, pycairo package removed.

--- asr/settings.py
+++ asr/settings.py
@@ -13,6 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+# print("Current working directory:", os.getcwd())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
