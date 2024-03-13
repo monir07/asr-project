08b085f718a3e08dcbbd1f9d9b05c5bd3b938274
monir07 <monirulslm7@gmail.com>
2024-03-13 07:36:43 +0600

    capital under score tamplate tag added.

--- tender/templatetags/custom_tag.py
+++ tender/templatetags/custom_tag.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def value_from_model(model, field):
     return getattr(model, field)
+
+@register.filter(name='capitalize_under')
+def capitalize_under(value):
+    value = value.replace('_', ' ')
+    return value.title()
